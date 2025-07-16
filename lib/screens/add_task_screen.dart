7ed9c772cebd5a3e99744bb0a715d29e70dcd2f5
Tester <tester@Testers-MacBook-Air.local>
2025-07-16 14:35:52 +0700

    Add .gitignore and initial project files

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import '../models/todo.dart';
+
+
+class AddTaskScreen extends StatefulWidget {
+ final Function(Todo) onAdd;
+
+
+ const AddTaskScreen({super.key, required this.onAdd});
+
+
+ @override
+ State<AddTaskScreen> createState() => _AddTaskScreenState();
+}
+
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+ final _titleController = TextEditingController();
+ final _descController = TextEditingController();
+ DateTime _selectedDate = DateTime.now();
+
+
+ void _submit() {
+   final title = _titleController.text.trim();
+   final desc = _descController.text.trim();
+
+
+   if (title.isEmpty || desc.isEmpty) return;
+
+
+   final todo = Todo(
+     title: title,
+     description: desc,
+     deadline: _selectedDate,
+   );
+
+
+   widget.onAdd(todo);
+   Navigator.pop(context);
+ }
+
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: const Text('Add task')),
+     body: Padding(
+       padding: const EdgeInsets.all(16),
+       child: Column(
+         children: [
+           TextField(
+             controller: _titleController,
+             decoration: const InputDecoration(labelText: 'Task title'),
+           ),
+           TextField(
+             controller: _descController,
+             decoration: const InputDecoration(labelText: 'Task description'),
+           ),
+           const SizedBox(height: 16),
+           Row(
+             children: [
+               Expanded(
+                 child: Text(
+                   'Deadline: ${_selectedDate.toLocal()}'.split('.')[0],
+                 ),
+               ),
+               TextButton(
+                 onPressed: () async {
+                   final picked = await showDatePicker(
+                     context: context,
+                     initialDate: _selectedDate,
+                     firstDate: DateTime.now(),
+                     lastDate: DateTime(2100),
+                   );
+                   if (picked != null) {
+                     setState(() {
+                       _selectedDate = picked;
+                     });
+                   }
+                 },
+                 child: const Text('Pick date'),
+               ),
+             ],
+           ),
+           const SizedBox(height: 24),
+           ElevatedButton(
+             onPressed: _submit,
+             child: const Text('Add task'),
+           )
+         ],
+       ),
+     ),
+   );
+ }
+}
